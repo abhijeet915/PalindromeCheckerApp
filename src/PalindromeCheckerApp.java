8acce638bc0a0e2220cab8cd7c17aaf9f4866e61
abhijeet915 <your-github-email@example.com>
2026-02-19 10:27:22 +0530

    UC3: Added palindrome check using string reverse loop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,16 +7,41 @@ public class PalindromeCheckerApp {
         System.out.println("      Version: 1.0");
         System.out.println("=================================\n");
 
-        // UC2 call
+        // UC2
         hardcodedPalindromeCheck();
+
+        // UC3
+        stringReversePalindromeCheck();
     }
 
+    // ==========================
+    // UC2: Hardcoded Palindrome
+    // ==========================
     public static void hardcodedPalindromeCheck() {
         String word = "madam";
         if (word.equals("madam")) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is NOT a palindrome.");
+        }
+    }
+
+    // ==========================
+    // UC3: String Reverse Palindrome
+    // ==========================
+    public static void stringReversePalindromeCheck() {
+        String original = "racecar";
+        String reversed = "";
+
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is NOT a palindrome.");
         }
     }
 }
