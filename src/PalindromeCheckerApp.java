d6d572cf5614aedfd1cf13984c20c2ee26f54ce5
abhijeet915 <your-github-email@example.com>
2026-02-19 10:25:08 +0530

    UC2: Added hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,22 @@
-import java.util.Scanner;
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("System initialized successfully");
 
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      Palindrome Checker App");
+        System.out.println("      Version: 1.0");
+        System.out.println("=================================\n");
+
+        // UC2 call
+        hardcodedPalindromeCheck();
+    }
+
+    public static void hardcodedPalindromeCheck() {
+        String word = "madam";
+        if (word.equals("madam")) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is NOT a palindrome.");
+        }
     }
 }
